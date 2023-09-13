d5b836c9bc0e8e76473b0ca9c8efab69e9c1efc9
BartvLaatum <hbpvanlaatum123@gmail.com>
2023-09-13 17:54:31 +0200

    created usefull util functions

--- experiments/ruleBasedController.py
+++ experiments/ruleBasedController.py
@@ -1,15 +1,18 @@
 import yaml
 from RLGreenLight.environments.GreenLight import GreenLight, runRuleBasedController
-from datetime import datetime
 from RLGreenLight.environments.pyutils import days2date
 import seaborn as sns; sns.set()
 import numpy as np
+import time
 
 def runNominalController(params, options):
     GL = GreenLight(**params, options=options, training=False)
 
     # time controller
+    start = time.time()
     states, controls, weather = runRuleBasedController(GL, options)
+    end = time.time()
+    print(f"Time to run controller: {end-start}")
     states["Time"] = np.asarray(days2date(states["Time"].values, "01-01-0001"), "datetime64[s]")
     controls["Time"] = states["Time"]
     weather["Time"] = states["Time"]
@@ -17,9 +20,9 @@ def runNominalController(params, options):
     # convert first date of time column to YYYYMMDD format
     dates = states["Time"].dt.strftime("%Y%m%d")
 
-    states.to_csv(f"data/ruleBasedControl/states{dates[0]}-{params['seasonLength']:03}.csv", index=False)
-    controls.to_csv(f"data/ruleBasedControl/controls{dates[0]}-{params['seasonLength']:03}.csv", index=False)
-    weather.to_csv(f"data/ruleBasedControl/weather{dates[0]}-{params['seasonLength']:03}.csv", index=False)
+    # states.to_csv(f"data/lessControls/new-states{dates[0]}-{params['seasonLength']:03}.csv", index=False)
+    # controls.to_csv(f"data/lessControls/new-controls{dates[0]}-{params['seasonLength']:03}.csv", index=False)
+    # weather.to_csv(f"data/lessControls/new-weather{dates[0]}-{params['seasonLength']:03}.csv", index=False)
 
 if __name__ == "__main__":
     with open("hyperparameters/ruleBasedControl.yml", "r") as f:
