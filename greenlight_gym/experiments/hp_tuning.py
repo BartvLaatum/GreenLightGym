1b1063a17de0f2e1ff2845ab909aac38a23b502c
BartvLaatum <hbpvanlaatum123@gmail.com>
2024-02-15 09:18:12 +0100

    hyperparameter tuning streamline

--- greenlight_gym/experiments/hp_tuning.py
+++ greenlight_gym/experiments/hp_tuning.py
@@ -4,6 +4,7 @@ import argparse
 from copy import copy
 from os.path import join
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import gc
 
 import wandb
 import numpy as np
@@ -11,13 +12,13 @@ from stable_baselines3 import PPO
 from wandb.integration.sb3 import WandbCallback
 
 from greenlight_gym.common.callbacks import TensorboardCallback
-from greenlight_gym.experiments.utils import loadParameters, make_vec_env, set_model_params
+from greenlight_gym.experiments.utils import load_env_params, load_model_params, make_vec_env, set_model_params
 
 #TODO WRITE CUSTOM EXPERIMENT FUNCTION FOR THIS PROCESS.
 def run_hp_experiment(
     env_id,
-    envBaseParams,
-    envSpecificParams,
+    env_base_params,
+    env_specific_params,
     options,
     modelParams,
     SEED,
@@ -26,40 +27,38 @@ def run_hp_experiment(
     project,
     total_timesteps,
     n_evals,
-    state_columns,
-    action_columns,
-    states2plot=None,
-    actions2plot=None,
     run=None
     ):
 
     monitor_filename = None
     vec_norm_kwargs = {"norm_obs": True, "norm_reward": True, "clip_obs": 50_000}
 
+
     eval_env = make_vec_env(
         env_id,
-        envBaseParams,
-        envSpecificParams,
+        env_base_params,
+        env_specific_params,
         options,
         seed=SEED,
-        numCpus=1,
+        n_envs=10,
         monitor_filename=monitor_filename,
         vec_norm_kwargs=vec_norm_kwargs,
         eval_env=True,
         )
 
     env_log_dir = f"train_data/{project}/envs/{run.name}/"
-    model_log_dir = f"train_data/{project}/models/{run.name}/"
     eval_freq = total_timesteps//n_evals//n_envs
+    # we don't save the best model here, we just want to evaluate the model
+    best_model_save_path = None
 
     save_name = "vec_norm"
     env = make_vec_env(
             args.env_id,
-            envBaseParams,
-            envSpecificParams,
+            env_base_params,
+            env_specific_params,
             options,
             seed=SEED,
-            numCpus=n_envs,
+            n_envs=n_envs,
             monitor_filename=monitor_filename,
             vec_norm_kwargs=vec_norm_kwargs
             )
@@ -68,16 +67,12 @@ def run_hp_experiment(
                                 eval_env,
                                 n_eval_episodes=n_eval_episodes,
                                 eval_freq=eval_freq,
-                                best_model_save_path=model_log_dir,
+                                best_model_save_path=best_model_save_path,
                                 name_vec_env=save_name,
                                 path_vec_env=env_log_dir,
                                 deterministic=True,
                                 callback_on_new_best=None,
                                 run=None,
-                                action_columns=action_columns,
-                                state_columns=state_columns,
-                                states2plot=None,
-                                actions2plot=None,
                                 verbose=1),
                                 WandbCallback(verbose=1)
                                 ]
@@ -96,21 +91,28 @@ def run_hp_experiment(
         total_timesteps=total_timesteps,
         callback=callbacks
         )
+    
+    # run.finish()
+    env.close()
+    eval_env.close()
+    del model
+    gc.collect()
+
 
 def train(config=None):
     with wandb.init(config=config, sync_tensorboard=True) as run:
         config = wandb.config
         modelParams =  set_model_params(config)
         def_config = copy(modelParams)
-        def_config['predHorizon'] = config["predHorizon"]
-        envBaseParams["predHorizon"] = config["predHorizon"]
+        def_config['pred_horizon'] = config["pred_horizon"]
+        env_base_params["pred_horizon"] = config["pred_horizon"]
 
         run.config.setdefaults(def_config)    
 
         run_hp_experiment(
             args.env_id,
-            envBaseParams,
-            envSpecificParams,
+            env_base_params,
+            env_specific_params,
             options,
             modelParams,
             SEED,
@@ -119,53 +121,44 @@ def train(config=None):
             args.project,
             total_timesteps=args.total_timesteps,
             n_evals=args.n_evals,
-            state_columns=state_columns,
-            action_columns=action_columns,
-            states2plot=None,
-            actions2plot=None,
             run=run,
             )
  
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--env_id", type=str, default="GreenLightHeatCO2")
-    parser.add_argument("--project", type=str, default="TestVecLoadSave")
+    parser.add_argument("--project", type=str, default="tuning-5-min")
     parser.add_argument("--group", type=str, default="testing-evaluation")
-    parser.add_argument("--HPfolder", type=str, default="gl_heat_co2")
-    parser.add_argument("--HPfilename", type=str, default="ppo_4_controls.yml")
-    parser.add_argument("--tuning_file", type=str, default="tuning.yml")
+    parser.add_argument("--env_config_name", type=str, default="5min_four_controls")
+    parser.add_argument("--tuning_file", type=str, default="5-min-four-controls.yml")
     parser.add_argument("--total_timesteps", type=int, default=1_000_000)
-    parser.add_argument("--n_eval_episodes", type=int, default=1)
-    parser.add_argument("--n_evals", type=int, default=10)
+    parser.add_argument("--n_eval_episodes", type=int, default=60)
+    parser.add_argument("--n_evals", type=int, default=1)
+    parser.add_argument("--method", type=str, default='bayes')
+    parser.add_argument("--algorithm", type=str, default="ppo")
     args = parser.parse_args()
 
     sweep_config = {
-        'method': 'bayes'
+        'method': args.method
         }
 
     metric = {
-        'name': 'rollout/ep_rew_mean',
+        'name': 'eval/mean_reward',
         'goal': 'maximize'
         }
 
-    early_terminate = {
-        "type": "hyperband",
-        "min_iter": 50,
-        "eta": 3,
-        }
     sweep_config['metric'] = metric
-    sweep_config["early_terminate"] = early_terminate
 
-    hpPath = f"hyperparameters/{args.HPfolder}/"
-    states2plot = ["Air Temperature","CO2 concentration", "Humidity", "Fruit harvest", "PAR", "Cumulative harvest"]
-    actions2plot = ["uBoil", "uCO2", "uThScr", "uVent", "uLamp"]
+    env_config_path = f"configs/envs/"
+    model_config_path = f"configs/algorithms/"
+    hp_path = f"configs/hp_tuning/"
 
     SEED = 666
     algorithm = "PPO"
-    envBaseParams, envSpecificParams, modelParams, options, state_columns, action_columns =\
-                            loadParameters(args.env_id, hpPath, args.HPfilename, algorithm)
+    env_base_params, env_specific_params, options, result_columns = load_env_params(args.env_id, env_config_path, args.env_config_name)
+    model_params = load_model_params(args.algorithm, model_config_path, args.env_config_name)
 
-    with open(join(hpPath, args.tuning_file), "r") as f:
+    with open(join(hp_path, args.tuning_file), "r") as f:
         params = yaml.load(f, Loader=yaml.FullLoader)
 
     parameters = params["parameters"]
