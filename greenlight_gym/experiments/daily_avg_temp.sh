32eaa13b3c818fade0461b8ada4991eeea4f29d4
BartvLaatum <hbpvanlaatum123@gmail.com>
2024-08-30 13:39:39 +0200

    moved deleted run scripts to main project folder

--- greenlight_gym/experiments/daily_avg_temp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Change to the directory containing the Python script
-cd /home/bart/Documents/phd-code-projects/GreenLightGym/greenlight_gym
-
-# Run the Python script
-python experiments/train_agent.py --env_id GreenLightHeatCO2 --project effect-daily-avg-temp --group additive-penalty-prev-controls --env_config_name additive_previous_control --algorithm ppo-98 --total_timesteps 20_000_000 --n_eval_episodes 60 --seed 666 --n_evals 20 --save_model --save_env
-python experiments/train_agent.py --env_id GreenLightHeatCO2 --project effect-daily-avg-temp --group multiplicative-penalty-prev-controls --env_config_name multiplicative_previous_control --algorithm ppo-98 --total_timesteps 20_000_000 --n_eval_episodes 60 --seed 666 --n_evals 20 --save_model --save_env
